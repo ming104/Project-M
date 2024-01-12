0303eca81eeddfaadcab0d7dc7088c54c24d37fa
ming104 <jangminguo@gmail.com>
2024-01-12 16:26:15 +0900

    직원FSM 생성, 회사내부 콜라이더, 복도 부서 나누기 했음

--- Assets/Script/S_Play/Managers/EmployeeManager.cs
+++ Assets/Script/S_Play/Managers/EmployeeManager.cs
@@ -1,7 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum EmployeeFSM
+{
+    Wait = 0,
+    moving = 1,
+    Work = 2,
+    battle = 3,
+    status_effect = 4
+}
 
 public class EmployeeManager : Singleton<EmployeeManager>
 {
@@ -19,8 +29,9 @@ public class EmployeeManager : Singleton<EmployeeManager>
             empdata._empName = Employees[i];
             empdata._empHp = DataManager.Instance.EmployeeDataLoad(Employees[i]).hp;
             empdata._empdef = DataManager.Instance.EmployeeDataLoad(Employees[i]).def;
+            empdata._empPower = DataManager.Instance.EmployeeDataLoad(Employees[i]).power;
             empdata._empintelligence = DataManager.Instance.EmployeeDataLoad(Employees[i]).intelligence;
-
+            empdata._empMovementSpeed = DataManager.Instance.EmployeeDataLoad(Employees[i]).movementSpeed;
         }
     }
 
